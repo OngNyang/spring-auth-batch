13d8b991605fa6cabc8bf2bcafe7e7f4742e9e99
OngNyang <108539087+OngNyang@users.noreply.github.com>
2025-01-01 23:54:48 +0900

    레디스 연결 코드 작성건

---
+++ src/main/java/org/example/springauthbatch/service/RedisSessionService.java
@@ -0,0 +1,23 @@
+package org.example.springauthbatch.service;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class RedisSessionService {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public RedisSessionService(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public Set<String>  getActiveSessions() {
+        return (redisTemplate.keys("spring:session:sessions:*"));
+    }
+
+    public Object   getSessionDetails(String seesionId) {
+        return (redisTemplate.opsForHash().entries(seesionId));
+    }
+}
