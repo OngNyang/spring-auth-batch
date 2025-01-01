6ad960eb9102d8e1f60e689a1ef0b66305204f9a
OngNyang <108539087+OngNyang@users.noreply.github.com>
2025-01-01 22:36:13 +0900

    Initial Commit

---
+++ src/main/java/org/example/springauthbatch/SpringAuthBatchApplication.java
@@ -0,0 +1,13 @@
+package org.example.springauthbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthBatchApplication.class, args);
+    }
+
+}
